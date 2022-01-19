4c330c2c339132185efe341c802a2bde69b7a4b1
Trever Cluney <chadcloney@yahoo.com>
2022-01-18 23:14:10 -0600

    Finished second project of model 1

---
+++ Module 1/FirstResponsiveWebAppCluney/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppCluney.Models;
+
+namespace FirstResponsiveWebAppCluney.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Result = "";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(ResponsiveModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Result = model.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.Result = "";
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Module 1/FirstResponsiveWebAppCluney/Models/ResponsiveModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppCluney.Models
+{
+    public class ResponsiveModel
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string? Name { get; set; }
+        public DateTime BirthYear { get; set; }
+        public string AgeThisYear()
+        {
+            DateTime temp = DateTime.Now;
+            DateTime endOfYear = new DateTime(temp.Year, 12, 31);
+            int age = endOfYear.Year - BirthYear.Year;
+            return String.Format("{0} will be {1} years old by December 31st of {2}",Name,age,endOfYear.Year);
+        }
+    }
+}
